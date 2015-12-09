091fc105e717ef92864d394d37bf725dd5723f26
Frederic Everaert <Frederic@imac.local>
2015-12-09 09:59:20 +0100

    - added MainViewController

---
+++ ChessNews/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  ChessNews
+//
+//  Created by Frederic Everaert on 9/12/15.
+//  Copyright © 2015 Frederic Everaert. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        let feedTableViewController = segue.destinationViewController as! FeedTableViewController
+        
+        // Pass the selected object to the new view controller.
+    }
+    
+}
