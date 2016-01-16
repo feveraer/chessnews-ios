1f4166b5647a17f1553e2599ff3e8e513317b321
Frederic Everaert <Frederic@imac.local>
2016-01-16 23:22:05 +0100

    - storyboard: label background - MainViewController: cleanup

--- ChessNews/MainViewController.swift
+++ ChessNews/MainViewController.swift
@@ -9,28 +9,9 @@
 import UIKit
 
 class MainViewController: UICollectionViewController {
-
-    // contains feed buttons
-    // @IBOutlet weak var feedsStack: UIStackView!
     
     var model = FeedsModel()
     var selectedFeed: Feed!
-    
-//    override func viewDidLoad() {
-//        // first remove template buttons from storyboard
-//        for view in feedsStack.arrangedSubviews {
-//            view.removeFromSuperview()
-//        }
-//        
-//        // add a button to feedsStack for each feed in model
-//        for feed in model.feeds {
-//            let button = UIButton(type: .System)
-//            button.setTitle(feed.name, forState: .Normal)
-//            button.addTarget(self, action: "choose:", forControlEvents: .TouchUpInside)
-//
-//            feedsStack.addArrangedSubview(button)
-//        }
-//    }
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // number of items in section = amount of feeds
@@ -45,17 +26,10 @@ class MainViewController: UICollectionViewController {
         let feed = model.feeds[indexPath.row]
         
         cell.feedImageView!.image = feed.image
-        cell.feedLabel!.text = feed.name
+        cell.feedLabel!.text = "  " + feed.name
         
         return cell
     }
-    
-    // fired when clicked on a feed button
-//    func choose(sender: UIButton) {
-//        selectedFeed = model.feeds.filter {
-//            $0.name == sender.titleLabel!.text }.first!
-//        performSegueWithIdentifier("choose", sender: self)
-//    }
     
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         selectedFeed = model.feeds[indexPath.row]
