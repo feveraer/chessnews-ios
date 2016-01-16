a9edb128c790ae736bfd604123c8d9577f3e7651
Frederic Everaert <Frederic@imac.local>
2016-01-16 01:44:43 +0100

    - storyboard: table view for rankings - added RankingsTableViewController

---
+++ ChessNews/RankingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RankingsTableViewController.swift
+//  ChessNews
+//
+//  Created by Frederic Everaert on 16/01/16.
+//  Copyright © 2016 Frederic Everaert. All rights reserved.
+//
+
+import UIKit
+
+class RankingsTableViewController: UITableViewController {
+    
+}
