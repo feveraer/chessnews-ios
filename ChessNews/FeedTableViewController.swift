b51220106203ef3e83ec83a5fbfe41f6ab569009
Frederic Everaert <Frederic@imac.local>
2016-01-16 11:13:30 +0100

    FeedTableViewController: cleanup

--- ChessNews/FeedTableViewController.swift
+++ ChessNews/FeedTableViewController.swift
@@ -12,80 +12,76 @@ import KINWebBrowser
 
 class FeedTableViewController: UITableViewController, MWFeedParserDelegate {
 
+    // the current feed
     var feed: Feed!
+    
+    // all items of the feed
     var feedItems = [MWFeedItem]()
     
-    //setup MWFeedParser
+    // setup MWFeedParser
     func request() {
         let url = NSURL(string: feed.url)
         let parser = MWFeedParser(feedURL: url)
         
+        // instance of FeedTableViewController confirms to MWFeedParserDelegate
         parser.delegate = self
+        
+        // download and parse the feed
         parser.parse()
     }
     
-    //MWFeedParser delegate
+    // MWFeedParser delegate functions
     
     func feedParserDidStart(parser: MWFeedParser!) {
-        //clear feed items
+        // clear feed items
         feedItems = [MWFeedItem]()
     }
     
     func feedParserDidFinish(parser: MWFeedParser!) {
-        //after parse, reload data tableview
+        // after parse, reload data tableview
         self.tableView.reloadData()
     }
     
-    // Provides info about the feed
+    // provides info about the feed
     func feedParser(parser: MWFeedParser!, didParseFeedInfo info: MWFeedInfo!) {
-        print(info)
-        //display feed title in navigation bar
+        // display feed title in navigation bar
         self.title = info.title
     }
     
-    // Provides info about a feed item
+    // provides info about a feed item
     func feedParser(parser: MWFeedParser!, didParseFeedItem item: MWFeedItem!) {
         feedItems.append(item)
     }
 
-    // MARK: - Table view data source
+    // Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        //return number of sections
+        // table view only contains 1 section
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return number of rows
+        // number of rows = amount of items in the feed
         return feedItems.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        // Configure the cell...
+        // configure the cell
         let item = feedItems[indexPath.row] as MWFeedItem
-        cell.textLabel?.text = item.title
+        cell.textLabel!.text = item.title
 
         return cell
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    //load the feed
+    // load the feed
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         request()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // When row is selected
+    // when row is selected
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         // get selected feed item
         let item = feedItems[indexPath.row] as MWFeedItem
@@ -101,40 +97,5 @@ class FeedTableViewController: UITableViewController, MWFeedParserDelegate {
         // push the browser on navigation controller stack
         self.navigationController?.pushViewController(browser, animated: true)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
 }
